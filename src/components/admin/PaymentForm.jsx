17beed87b3d947d88c212b09de5eec5f0cf5117b
Huy005-ui <quochuydaonguyen@gmail.com>
2026-06-23 01:09:26 +0700

    FixPayment

--- src/components/admin/PaymentForm.jsx
+++ src/components/admin/PaymentForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { toast } from "react-toastify";
+import adminBookingService from "../../services/adminBookingService";
 export default function PaymentForm({
   booking,
   onSuccess,
