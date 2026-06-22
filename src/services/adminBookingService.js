611bacdbed82913e44042a1c711bf6a8a889c9c8
Huy005-ui <quochuydaonguyen@gmail.com>
2026-06-23 00:08:53 +0700

    FixadminBookingService

--- src/services/adminBookingService.js
+++ src/services/adminBookingService.js
@@ -1,20 +1,20 @@
-import axiosInstance from "../api/axiosInstance";
+import adminAxiosInstance from "../api/adminAxiosInstance";
 
 const adminBookingService = {
   getAll(params) {
-    return axiosInstance.get("/admin/bookings", { params });
+    return adminAxiosInstance.get("/admin/bookings", { params });
   },
 
   getDetail(id) {
-    return axiosInstance.get(`/admin/bookings/${id}`);
+    return adminAxiosInstance.get(`/admin/bookings/${id}`);
   },
 
   updateStatus(id, status) {
-    return axiosInstance.patch(`/admin/bookings/${id}/status`, { status });
+    return adminAxiosInstance.patch(`/admin/bookings/${id}/status`, { status });
   },
 
   payment(id, payload) {
-    return axiosInstance.post(`/admin/bookings/${id}/payment`, payload);
+    return adminAxiosInstance.post(`/admin/bookings/${id}/payment`, payload);
   },
 };
 
