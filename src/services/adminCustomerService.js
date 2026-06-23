87152a1fc7a9f66d698946b5bbcebd900417c4e8
Huy005-ui <quochuydaonguyen@gmail.com>
2026-06-24 02:23:34 +0700

    FixAdminCustomerService

--- src/services/adminCustomerService.js
+++ src/services/adminCustomerService.js
@@ -1,4 +1,5 @@
-import axiosInstance from '../api/axiosInstance';
+import axios from "axios";
+
 
 const adminAxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5001',
